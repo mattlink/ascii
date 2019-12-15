bea24c20c0e36c7be993da77f37589bff470950a
mattlink <mattlink570@gmail.com>
2019-12-14 20:34:25 -0600

    start a Cave implementation of Room

---
+++ src/Entity/Rooms/Cave.ts
@@ -0,0 +1,12 @@
+import { Room } from './Room';
+
+export class Cave extends Room {
+
+    constructor(width: number, height: number) {
+        super(width, height);
+    }
+
+    init () {
+        
+    }
+}
