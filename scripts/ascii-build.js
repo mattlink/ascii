f5d73861edf453a62b9a3d4d277d4778a2bd6bc5
mattlink <mattlink570@gmail.com>
2020-02-14 13:47:54 -0600

    check if require statements are working on other systems

--- scripts/ascii-build.js
+++ scripts/ascii-build.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
+const ts = require('typescript');
+console.log(ts);
+
+const browserify = require('browserify');
+console.log(browserify);
 // used for building a game which uses this package
 console.log('ascii-build coming soon');
